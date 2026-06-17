e260d5f74fd2e5ef3af500e7d1c530264af448d0
dayalshreyansh <shreyansh.dayal0604@gmail.com>
2026-06-17 13:13:41 +0530

    Updated website 2

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -30,6 +30,7 @@ function Projects() {
                 </div>
               </div>
             </div>
+            <div data-aos="fade-left" className="my-20">
             <div className="h-[500px] w-[80vw] max-w-[500px] lg:h-[550px] lg:w-[500px] bg-[#222831] ">
               <img className="" src={ellora} alt="" />
               <div className="text-[#f2f2f2] p-3 mx-2 text-xl lg:text-2xl font-semibold">
